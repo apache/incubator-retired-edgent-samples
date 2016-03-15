e98b39caf1c2fde9bd394b5a4f8b2bf331fc0f63
Queenie Ma <queeniema.apache@gmail.com>
2016-03-15 16:01:39 -0700

    Add ASF license headers and add reasoning for a line of code

--- topology/src/main/java/quarks/samples/topology/CombiningStreamsProcessingResults.java
+++ topology/src/main/java/quarks/samples/topology/CombiningStreamsProcessingResults.java
@@ -1,8 +1,21 @@
 /*
-# Licensed Materials - Property of IBM
-# Copyright IBM Corp. 2015,2016
-*/
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
 
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied.  See the License for the
+specific language governing permissions and limitations
+under the License.
+*/
 package quarks.samples.topology;
 
 import java.util.HashSet;
@@ -136,8 +149,8 @@ public class CombiningStreamsProcessingResults {
                 .modify(tuple -> "Hypertensive Crisis!!!\n" + tuple)
                 .tag("hypertensive");
 
-        // Union two streams to obtain a single stream containing alerts from the normal and
-        // prehypertension alert streams
+        // Additional processing for these streams could go here. In this case, union two streams
+        // to obtain a single stream containing alerts from the normal and prehypertension alert streams.
         TStream<String> normalAndPrehypertensionAlerts = normalAlerts.union(prehypertensionAlerts);
 
         // Set of streams containing alerts from the other categories

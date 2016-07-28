8a12630b3c52989903e75036f3c33fd8d333a3f2
cazen.lee <cazen.lee@samsung.com>
2016-07-28 10:21:19 +0900

    chz package name from iotf to iotp

--- scenarios/src/main/java/org/apache/edgent/samples/scenarios/iotp/range/sensor/LED.java
+++ scenarios/src/main/java/org/apache/edgent/samples/scenarios/iotp/range/sensor/LED.java
@@ -16,7 +16,7 @@ KIND, either express or implied.  See the License for the
 specific language governing permissions and limitations
 under the License.
 */
-package org.apache.edgent.samples.scenarios.iotf.range.sensor;
+package org.apache.edgent.samples.scenarios.iotp.range.sensor;
 
 import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioFactory;
